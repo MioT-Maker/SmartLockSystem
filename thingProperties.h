634e9dee23d0ea03a6fad97bba19f0fab4126d3d
MioT-Maker <miotmaker@gmail.com>
2026-06-04 01:43:17 +0900

    Add files via upload

---
+++ thingProperties.h
@@ -0,0 +1,30 @@
+#include <ArduinoIoTCloud.h>
+#include <Arduino_ConnectionHandler.h>
+#include <Arduino_NetworkConfigurator.h>
+#include "configuratorAgents/agents/BLEAgent.h"
+#include "configuratorAgents/agents/SerialAgent.h"
+void onCardUUIDChange();
+
+String CardUUID;
+CloudTemperatureSensor DeviceTemperature;
+bool DoorOpen;
+
+KVStore kvStore;
+BLEAgentClass BLEAgent;
+SerialAgentClass SerialAgent;
+WiFiConnectionHandler ArduinoIoTPreferredConnection; 
+NetworkConfiguratorClass NetworkConfigurator(ArduinoIoTPreferredConnection);
+
+void initProperties(){
+  NetworkConfigurator.addAgent(BLEAgent);
+  NetworkConfigurator.addAgent(SerialAgent);
+  NetworkConfigurator.setStorage(kvStore);
+  // For changing the default reset pin uncomment and set your preferred pin. Use DISABLE_PIN for disabling the reset procedure.
+  //NetworkConfigurator.setReconfigurePin(your_pin);
+  ArduinoCloud.setConfigurator(NetworkConfigurator);
+
+  ArduinoCloud.addProperty(CardUUID, READWRITE, 1 * SECONDS, onCardUUIDChange);
+  ArduinoCloud.addProperty(DeviceTemperature, READ, 1 * SECONDS, NULL);
+  ArduinoCloud.addProperty(DoorOpen, READ, 1 * SECONDS, NULL);
+
+}
